51d88549dd205899c00d9b98955234e55f073b55
Suraj Bhargav <surajbhargav261998@gmail.com>
2026-05-08 17:02:05 +0530

    s

---
+++ itechglobal-theme/page-home.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: Home (Itech Global)
+*/
+get_header();
+get_template_part('template-parts/home', 'content');
+get_footer();
---
+++ theme-package/itechglobal-theme/index.php
@@ -0,0 +1,5 @@
+<?php
+// Fallback template required by WordPress.
+get_header();
+get_template_part('template-parts/home', 'content');
+get_footer();
