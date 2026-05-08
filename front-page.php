1fe18f90ff251b93137d06ef77c49b282e19132e
Suraj Bhargav <surajbhargav261998@gmail.com>
2026-05-08 17:04:46 +0530

    dele

--- front-page.php
+++
@@ -1,4 +0,0 @@
-<?php
-get_header();
-get_template_part('template-parts/home', 'content');
-get_footer();
--- itechglobal-theme/page-home.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-Template Name: Home (Itech Global)
-*/
-get_header();
-get_template_part('template-parts/home', 'content');
-get_footer();
